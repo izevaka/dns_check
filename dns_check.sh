c27f25db6d8fbb89cb13aab704cfc46b48eebb28
Igor Zevaka <izevaka@gmail.com>
2013-06-11 10:49:02 +1000

    Added comment for fallback IP

--- dns_check.sh
+++ dns_check.sh
@@ -4,15 +4,18 @@
 # a specified host by resolving another host.
 ############################################################################
 
-BASE_FILE=`dirname $0`/dnsmasq.base.conf
-TARGET_FILE=/etc/dnsmasq.conf
-FALLBACK_IP=125.56.205.24
-LOG="logger -t `basename $0`"
-
 # This is the host we want to resolve to an IP address of $SOURCE_HOST
 RESOLVE_HOST=akamaihd.net
 # This is the host whose IP address we want to use for misbehaving hosts
 SOURCE_HOST=trailers.apple.com
+# This address gets used when $SOURCE_HOST can't be resolved. Set it to
+# an IP address of trailers.apple.com that you get on your network.
+# This particular address is for Optus network
+FALLBACK_IP=125.56.205.24
+
+BASE_FILE=`dirname $0`/dnsmasq.base.conf
+TARGET_FILE=/etc/dnsmasq.conf
+LOG="logger -t `basename $0`"
 
 # Pick the first server out of nslookup for $SOURCE_HOST
 new_ip=`nslookup $SOURCE_HOST | grep "Address 1:" | grep -v localhost | cut -d' ' -f3 | head -n 1`
